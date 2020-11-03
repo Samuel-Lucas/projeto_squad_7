b05381e64d1e3d19a70539f3747711b95321ecad
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2020-11-02 23:16:22 -0300

    Validação login

--- painel.php
+++ painel.php
@@ -2,5 +2,6 @@
         require_once "php/actions/conexao.php";
         include_once "php/includes/head.php";
         include_once "php/includes/header.php";
+        include "php/actions/verifica_login.php";
 
 ?>
--- php/actions/logar.php
+++ php/actions/logar.php
@@ -3,6 +3,10 @@
 
         session_start();
 
+        if(empty($_POST['email']) || empty($_POST['senha'])) {
+            header("Location: ../../login.php");
+        }
+
         if(isset($_POST['email']) && isset($_POST['senha'])) {
 
             $email = $_POST['email'];
@@ -11,6 +15,14 @@
             $sql = "select * from usuarios where email = '$email' and senha = md5('$senha') limit 1";
 
             $result = $conn->query($sql);
+
+            $row = mysqli_num_rows($result);
+
+            if($row != 1) {
+                $_SESSION['nao_autenticado'] = true;
+                header("Location: ../../login.php");
+                exit();
+            }
 
             while($row = mysqli_fetch_assoc($result)) {
                 if ($row['id_usuario']) {
